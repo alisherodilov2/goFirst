3e56e84248ba060a6875b780ff98be04922c8bd1
alisherodilov005 <93549542+alisherodilov2@users.noreply.github.com>
2024-07-11 18:31:07 +0500

    chnages

--- index.go
+++ index.go
@@ -1,48 +1,17 @@
 package main
-import (
-	"fmt"
-	
-)
-
-// const data = 2
-// func main() {
-// // 	var student1 string
-// //   student1 = "John"
-//     // var a, b int = 10, 3
-// 	var arr1 = [...]int{1 ,2,3}
-// 	if len(arr1) > 1{
-// 		fmt.Println(arr1)
-// 	}
-// }
-type Person struct {
-	name string
-	age int
-	job string
-	salary int
+import ("fmt")
+func testcount(x int) int {
+	if x == 30 {
+	  return 0
+	}
+	fmt.Println(x)
+	return testcount(x + 1)
   }
-  func getArray(){
-	var a = map[string]string{"brand":"Ford", "model":"Mustang"}
-	
-	fmt.Printf("%v\n", a["brand"])
+func main() {
+	testcount(1)
+	//  array_name := []string{"salom","name","kino"}
 
-  }
-  
-func main(){
-	getArray()
-	// var person1 Person
-	// person1.name="Alisher"
-	// person1.age=19
-	// person1.job = "Developer"
-	// person1.salary = 6
-
-	// fmt.Println("Name: ", person1.name)
-	// fmt.Println("Age: ", person1.age)
-	// fmt.Println("Job: ", person1.job)
-	// fmt.Println("Salary: ", person1.salary)
-
-	// var count int ;
-	// count = 4;
-	// for i := 0; i < count; i++ {
-	// 	fmt.Println("salom" + strconv.Itoa(i))
-	// }
-}
+	// for i:=0; i < len(array_name); i++ {
+	// 	fmt.Println(array_name[i])
+	//   }
+}	
