42e15aacff876720dd7d0a7e140be966e1b5942d
alisherodilov005 <93549542+alisherodilov2@users.noreply.github.com>
2024-07-15 17:40:15 +0500

    server

--- index.go
+++ index.go
@@ -1,17 +1,47 @@
-package main
-import ("fmt")
-func testcount(x int) int {
-	if x == 30 {
-	  return 0
-	}
-	fmt.Println(x)
-	return testcount(x + 1)
-  }
-func main() {
-	testcount(1)
-	//  array_name := []string{"salom","name","kino"}
+// package main
 
-	// for i:=0; i < len(array_name); i++ {
-	// 	fmt.Println(array_name[i])
-	//   }
-}	
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// )
+
+// func testcount(x int) int {
+// 	if x == 30 {
+// 		return 0
+// 	}
+// 	fmt.Println(x)
+// 	return testcount(x + 1)
+// }
+// func requestSend() {
+// 	resp, err := http.Get("https://music-api-inky-xi.vercel.app/artist")
+// 	defer resp.Body.Close()
+
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(string(body))
+// }
+// func main() {
+// 	requestSend()
+// 	// testcount(1)
+
+// }
+package main
+
+import (
+ "fmt"
+ "log"
+ "net/http"
+)
+
+func main() {
+ http.HandleFunc("/main", handler)
+ log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+ fmt.Fprint(w, "Hello, World!")
+}
